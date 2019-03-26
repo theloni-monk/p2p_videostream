ac1da6f6d53f166496896e898083953671417f01
Theo <theoac2009@outlook.com>
2019-03-25 23:27:59 -0700

    fixed Server proof of concept

--- devVenv/_code/Proofs/ServerProof.py
+++ devVenv/_code/Proofs/ServerProof.py
@@ -4,14 +4,14 @@ import cv2
 cam = camera.Camera(mirror=True)
 
 def retrieveImage(cam, imgResize):
-    """Basic function for retrieving camera data, for getFrame"""
-    img= cv2.resize(cam.image, (0, 0), fx=imgResize[0], fy=imgResize[1])    
-    return img
-    
+        """Basic function for retrieving camera data, for getFrame"""
+        img= cv2.resize(cam.image, (0, 0), fx=imgResize[0], fy=imgResize[1])    
+        return img
+
 def startStream(serv, getFrame, args=[]):
-    serv.initializeStream(serv.fetchFrame(getFrame, args))
-    while True:
-        serv.sendFrame(serv.fetchFrame(getFrame, args))
+        serv.initializeStream(getFrame(args))
+        while True:
+                serv.sendFrame(getFrame(args))
 
 resize_cof = (1,1)  
 server = streamserver.Server(port=5000, verbose=True)
