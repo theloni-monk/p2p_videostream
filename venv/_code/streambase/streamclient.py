26167e9daea1b6981583a1bb689d99a7067a4ebe
Theo <theo.acooper@gmail.com>
2019-03-30 00:33:04 -0700

    renamed virtual environment directory form devVenv to venv

---
+++ venv/_code/streambase/streamclient.py
@@ -0,0 +1,136 @@
+import socket
+from .netutils import *
+import cv2
+import numpy as np
+import zstandard
+import io
+import atexit
+
+
+class Client:
+    """Client class for videostreamer, decodes compressed and diffed images"""
+
+    def __init__(self, target_ip, **kwargs):
+
+        self.verbose = kwargs.get("verbose", False)
+
+        self.target_ip = target_ip
+        self.port = kwargs.get("port", 8080)
+        self.s = None
+        self.connected = False
+
+        # instanciate a decompressor which we can use to decompress our frames
+        self.D = zstandard.ZstdDecompressor()
+
+        # when the user exits or the stream crashes it closes so there arn't orfaned processes
+        atexit.register(self.close)
+        self.error=None
+        self.prevFrame = None
+        self.frameno = None
+        self.log("Client Ready")
+
+    def log(self, m):
+        """prints if self.verbose"""
+        if self.verbose:
+            print(m)  # printout if server is in verbose mode
+
+    def recv(self, size=1024):
+        # NOTE: this just works
+        """Recieves a single frame
+        args:
+            size: how big a frame should be
+                default: 1024 
+        returns:
+            single data frame
+        """
+        data = bytearray()
+        while 1:
+            buffer = self.s.recv(size)
+            data += buffer
+            if len(buffer) == size:
+                pass
+            else:
+                return data
+
+    def initializeSock(self, sock=None, **kwargs):
+        """Setter for self.s socket or makes blank socket"""
+        if not sock:
+            # creates socket
+            self.log("Initializing socket...")
+            self.s = socket.socket()
+            self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            #self.s.bind((kwargs.get("bindto", ""), self.port))
+        else:
+            self.s = sock
+
+    def connectSock(self):
+        """ Connects socket to self.target_ip over self.port
+            returns: True on connection, False on failed connection """
+        # TODO: make encryption handshake
+        self.log("Connecting...")
+        try:
+            self.s.connect((self.target_ip, self.port))
+            self.connected = True
+        except ConnectionRefusedError:
+            self.log("connection refused")
+            self.connected = False
+            return False
+        return True
+
+    def initializeStream(self):
+        """Initializes and connects socket if uninitalized and receives initial frame"""
+
+        if not self.s:
+            self.initializeSock()  # if socket wasn't created make it now
+        if not self.connected:
+            self.connectSock()  # if socket wasn't connected connect now
+
+        # initial frame cant use intra-frame compression
+        self.prevFrame = np.load(io.BytesIO(
+            self.D.decompress(recv_msg(self.s))))
+        self.frameno = 0
+        self.log("stream initialized")
+
+    def decodeFrame(self):
+        """Decodes single frame of data from an initialized stream"""
+        try:
+            r = recv_msg(self.s)  # gets the frame difference
+        except Exception as e:
+            self.close(e)
+
+        try:
+            if len(r) == 0:
+                pass
+        except Exception as e:
+            self.close(Exception("Server sent Null data"))
+
+        # load decompressed image
+            # np.load creates an array from the serialized data
+        img = (np.load(io.BytesIO(self.D.decompress(r)))  # decompress the incoming frame difference
+               + self.prevFrame).astype("uint8")  # add the difference to the previous frame and convert to uint8 for safety
+
+        self.log("recieved {}KB (frame {})".format(
+            int(len(r)/1000), self.frameno))  # debugging
+        self.frameno += 1
+
+        self.prevFrame = img  # save the frame
+
+        return img
+
+    def close(self, E=None, **kwargs):
+        """Closes socket and opencv instances"""
+        if self.s:
+            self.s.close()
+
+        if(E != None):
+            self.error=E
+            print("Stream closed on Error\n" + str(E))
+            if kwargs.get("elevateErrors",False):
+                raise E
+        else:
+            self.log("Stream closed")
+        
+        if kwargs.get("destroy", False) == True:
+            self.log("Destroying self")
+            del self
+
---
+++ venv/_code/streambase/streamserver.py
@@ -0,0 +1,140 @@
+import socket
+from .netutils import *
+import numpy as np
+import io
+from tempfile import TemporaryFile
+import zstandard
+import atexit
+
+
+class Server:
+    """Server class for videostreamer, encodes frames and diffs them before sending"""
+
+    def __init__(self, incoming_ip, **kwargs):
+        self.verbose = kwargs.get("verbose", False)
+
+        self.port = kwargs.get("port", 8080)
+        self.incoming_ip = incoming_ip
+        self.connected = False
+        self.s = None
+        atexit.register(self.close)
+        self.error=None
+        self.elevateErrors = kwargs.get("elevateErrors", False)
+        self.log("Server ready")
+
+    def log(self, m):
+        """Prints out if verbose"""
+        if self.verbose:
+            print(m)  # printout if verbose
+
+    def initializeSock(self, sock=None, **kwargs):
+        self.log("Initilizing socket")
+        if not sock:
+            s = socket.socket()
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((kwargs.get("bindto", ""), self.port))
+            self.s = s
+            self.s.listen(10)
+        else:
+            self.s = sock
+
+    def serve(self):
+        """Blocks and waits for client at self.incoming_ip to connect"""
+
+        self.log("Searching for client at {}...".format(self.incoming_ip))
+        if not self.s:
+            self.initializeSock()
+        while True:
+            # wait for client to query the server for a connection
+            conn, clientAddr = self.s.accept()
+            if clientAddr[0] == self.incoming_ip:
+                self.conn = conn
+                self.clientAddr = clientAddr
+                self.log('Connected to ' +
+                         self.clientAddr[0] + ':' + str(self.clientAddr[1]))
+                return None  # only connects to one client
+            conn.close()
+            self.log('Refused connection to ' +
+                     clientAddr[0] + ':' + str(clientAddr[1]))
+
+    def serveNoBlock(self, callback=None):
+        """Without blocking, waits for client at self.incoming_ip to connect, if callback given calls callback with arg True on success or False on failure
+        Returns: False on failure, True on success"""
+
+        self.log("Searching for client at {}...".format(self.incoming_ip))
+        if not self.s:
+            self.initializeSock()
+        # wait for client to query the server for a connection
+        conn, clientAddr = self.s.accept()
+        if clientAddr[0] == self.incoming_ip:
+            self.conn = conn
+            self.clientAddr = clientAddr
+            self.log('Connected to ' +
+                     self.clientAddr[0] + ':' + str(self.clientAddr[1]))
+            if callback:
+                callback(True)
+            return True  # only connects to one client
+        conn.close()
+        self.log("serveNoBlock Failed!")
+        self.log('Refused connection to ' +
+                 clientAddr[0] + ':' + str(clientAddr[1]))
+        if callback:
+            callback(False)
+        return False
+
+    def initializeStream(self, img):
+        """Sends initial frame of compression and initializes compressor and io"""
+        self.Sfile = io.BytesIO()
+        self.C = zstandard.ZstdCompressor()
+        self.prevFrame = img
+        np.save(self.Sfile, self.prevFrame)
+        send_msg(self.conn, self.C.compress(self.Sfile.getvalue()))
+        self.frameno = 0
+
+    def sendFrame(self, img):
+        """Sends single frame with intra-frame compression over an initialized stream"""
+        try:
+            self.prevFrame
+        except AttributeError:
+            self.initializeStream()
+
+        # instanciate temporary bytearray to send later
+        Tfile = io.BytesIO()
+
+        # use numpys built in save function to diff with prevframe
+        # because we diff it it will compress more
+        np.save(Tfile, img-self.prevFrame)
+
+        # compress it into even less bytes
+        b = self.C.compress(Tfile.getvalue())
+
+        # saving prev frame
+        self.prevFrame = img
+
+        # send it
+        try:
+            send_msg(self.conn, b)
+        except Exception as e:
+            self.log("failed to send message")
+            self.close(e)
+        self.log("Sent {}KB (frame {})".format(
+            int(len(b)/1000), self.frameno))  # debugging
+        self.frameno += 1
+
+    def close(self, E=None, **kwargs):
+        """Closes socket"""
+        if self.s:
+            self.s.close()
+
+        if(E != None):
+            self.error=E
+            print("Stream closed on Error\n" + str(E))
+            if self.elevateErrors:
+                self.log("raising error")
+                raise E
+        else:
+            self.log("Stream closed")
+        
+        if kwargs.get("destroy", False) == True:
+            self.log("Deleting self")
+            del self
