69879fedf0eb78bedf988c349a459c4a1eac2d16
Theo <theoac2009@outlook.com>
2019-05-16 20:43:37 -0700

    audio proof

---
+++ venv/_code/Proofs/AudioPoof.py
@@ -0,0 +1,24 @@
+from .. import AudioStreamer
+import sys
+
+if __name__ == "main":
+    streamer= AudioStreamer.AudioStreamer(verbose = True, _diffmin = 0)
+    streamer.set_partner(("192.168.0.241", 5000))
+    if not streamer.connectPartner(): 
+        print("connectPartner failed")
+        sys.exit(0)
+    
+    streamer.init_infoExchange()
+    streamer.beginStreaming()
+    print("test1")
+    while True:
+            if not streamer.errorQueue_c.empty():
+                print("\nclientThread closed on error: {}\n".format(streamer.errorQueue_c.get(block=True, timeout= 2)))
+                break
+            if not streamer.errorQueue_p.empty():
+                print("\nplayerThread closed on error: {}\n".format(streamer.errorQueue_p.get(block=True, timeout= 2)))
+                break
+
+            if cv2.waitKey(1) == 27:
+                break  # esc to quit
+    streamer.close(destroy = True)
